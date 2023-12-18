b8a86bf287a8d1b0a3a137e5b2b9ff24a529b1e7
Alexander Linder <alexander.linder@mondcarion.group>
2023-12-18 23:03:57 +0100

    Add nix shell

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+  in {
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ pkgs.android-studio ];
+    };
+
+  };
+}
